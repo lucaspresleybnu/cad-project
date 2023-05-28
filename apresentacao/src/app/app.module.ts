cad5280fa620de1d6645ff58e45036ef6b8bb5e5
Lucas P. A. da Silva <lucas.presley.bnu@gmail.com>
2023-05-28 10:01:16 -0300

    Inclusão de novos componentes e angular material

--- apresentacao/src/app/app.module.ts
+++ apresentacao/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConsultaComponent } from './clientes/consulta/consulta.component';
+import { CadastroComponent } from './clientes/cadastro/cadastro.component';
+import { TabelaComponent } from './clientes/components/tabela/tabela.component';
+import { FormularioComponent } from './clientes/components/formulario/formulario.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultaComponent,
+    CadastroComponent,
+    TabelaComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
